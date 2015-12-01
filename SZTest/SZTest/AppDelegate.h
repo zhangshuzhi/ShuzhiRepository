cfce3c8bbc83149d2f2c0ce4180aa6cdfae56133
zhangshuzhi <17876852135@163.com>
2015-12-01 10:06:23 +0800

    第一次提交

---
+++ SZTest/SZTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SZTest
+//
+//  Created by tarena01 on 15/12/1.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
